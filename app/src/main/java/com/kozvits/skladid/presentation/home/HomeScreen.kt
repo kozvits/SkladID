4d400e9017d3daf220a5b5119391b2d62619b052
kozvits <kozvits@gmail.com>
2026-09-12 17:03:55 +0300

    Update HomeScreen.kt

--- app/src/main/java/com/kozvits/skladid/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/kozvits/skladid/presentation/home/HomeScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -44,7 +45,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.size
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.kozvits.skladid.R
 import com.kozvits.skladid.domain.model.Product
